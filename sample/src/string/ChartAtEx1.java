cc741e4d3bead186a93f50b45b7a446bed4d6261
root <root@localhost>
2024-06-08 21:12:23 +0530

    commit check

---
+++ sample/src/string/ChartAtEx1.java
@@ -0,0 +1,12 @@
+package string;
+
+public class ChartAtEx1 {
+
+	public static void main(String[] args) {
+		String word = "Education";
+		char postion = word.charAt(3);
+		System.out.println(postion + " is at position 3");
+
+	}
+
+}
