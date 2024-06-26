3bd404871612627dbdb59ffa045217dfcf0c61de
USER <USER@192.168.1.45>
2024-06-26 07:38:48 +0530

    NewProjectForJenkins_26June

---
+++ MavenProjectForJenkins/src/main/java/package1/SampleClass.java
@@ -0,0 +1,18 @@
+package package1;
+
+public class SampleClass {
+	
+	public static void add()
+	{
+		int a =6;
+		int b=7;
+		int c= a+b;
+		System.out.println("Sum is: " + c);
+	}
+
+	public static void main(String[] args) 
+	{
+		add();
+	}
+
+}
